081dbc09ba89c4acb351425487ac1833d9c8d73f
xyzuan <xyzuannihboss@gmail.com>
2024-01-01 14:43:49 +0700

    main: Init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:space_shooter_workshop/configs/firebase_options.dart';
 import 'package:space_shooter_workshop/main_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
